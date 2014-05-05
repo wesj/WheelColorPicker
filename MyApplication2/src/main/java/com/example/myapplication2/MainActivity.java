27c4c2868a562989e521cca67542f586db3eb9c0
Wes Johnston <wesj@localhost.localdomain>
2014-05-04 23:39:37 -0700

    Fix build to not require support v7

--- MyApplication2/src/main/java/com/example/myapplication2/MainActivity.java
+++ MyApplication2/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.myapplication2;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,18 +11,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                (new AlertDialog.Builder(MainActivity.this))
+                        .setTitle("Pick a color")
+                        .setView(new AdvancedColorPicker(MainActivity.this))
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();;
+                            }
+                        }).create()
+                        .show();
+            }
+        });
     }
 
 
@@ -45,40 +55,6 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            Button b = (Button) rootView.findViewById(R.id.button);
-            b.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    (new AlertDialog.Builder(getActivity()))
-                            .setTitle("Pick a color")
-                            .setView(new AdvancedColorPicker(getActivity()))
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();;
-                                }
-                            }).create()
-                            .show();
-                }
-            });
-
-            return rootView;
-        }
     }
 
 }
